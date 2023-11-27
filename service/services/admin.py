9e97c9177755363b0eb392a43703642f273ced7b
Sarkisyan Artak <sarkisyan.artak92@gmail.com>
2023-11-27 17:02:22 +0300

    Day 3 - 1

--- service/services/admin.py
+++ service/services/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from services.models import Subscriptions, Service, Plan
+from services.models import Service, Plan, Subscription
 
 admin.site.register(Service)
 admin.site.register(Plan)
-admin.site.register(Subscriptions)
+admin.site.register(Subscription)
--- service/services/models.py
+++ service/services/models.py
@@ -30,7 +30,7 @@ class Plan(models.Model):
         return self.plan_type
 
 
-class Subscriptions(models.Model):
+class Subscription(models.Model):
     client = models.ForeignKey(Client, related_name='subscriptions', on_delete=models.PROTECT)
     service = models.ForeignKey(Service, related_name='subscriptions', on_delete=models.PROTECT)
     plan = models.ForeignKey(Plan, related_name='subscriptions', on_delete=models.PROTECT)
